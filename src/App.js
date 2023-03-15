1b02d9eedf5c9d232ebf7edfa3d905ec2ab162bf
OssFit <oalatrista@gmail.com>
2023-03-14 20:16:19 -0500

    Add section for Soft Skills

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills1 } from "./components/Skills1";
+import { Skills2 } from "./components/Skills2";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills1 />
+      <Skills2 />
       <Projects />
       <Contact />
       <Footer />
