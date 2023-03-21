2c8155255020284786a1e10028550c11cc282dca
OssFit <oalatrista@gmail.com>
2023-03-20 23:46:38 -0500

    mailer ready

--- api/server.js
+++ api/server.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+require('dotenv').config();
+import express, { Router, json } from "express";
+const router = Router();
+import cors from "cors";
+import { createTransport } from "nodemailer";
 
 // server used to send send emails
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
-const contactEmail = nodemailer.createTransport({
+
+const contactEmail = createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "oalatrista@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
