71192a9811a2a4f91b7f25217d6a3a8de95c177f
OssFit <oalatrista@gmail.com>
2023-03-21 00:52:28 -0500

    fixing cors 3

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ app.use(cors({
   origin: 'https://portfolio-ossfit1.vercel.app'
 }));
 app.use(json());
-app.use("/", router);
+app.use("https://portfolio-api-one.vercel.app", router);
 const port=process.env.PORT || 5000;
 app.listen(port, () => console.log("Server Running"));
 
